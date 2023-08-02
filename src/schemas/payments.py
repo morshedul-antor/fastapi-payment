dba7ef2d0649f2e1f6d6e7393a82d4cef97fa12e
morshedul-antor <antor.morshedul@gmail.com>
2023-08-03 02:06:48 +0600

    add: payments

---
+++ src/schemas/payments.py
@@ -0,0 +1,33 @@
+from lib2to3.pgen2.token import OP
+from optparse import Option
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+
+class PaymentBase(BaseModel):
+    order_amount: Optional[str] = None
+    payment_status: Optional[str] = None
+    remarks: Optional[str] = None
+
+
+class PaymentIn(PaymentBase):
+    trx_id: Optional[str] = None
+    receiver_id: Optional[int] = None
+
+
+class PaymentDBIn(PaymentIn):
+    service_id: Optional[int] = None
+
+
+class PaymentOut(PaymentIn):
+    id: Optional[int] = None
+    service_id: Optional[int] = None
+    created_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+
+
+class PaymentUpdate(PaymentBase):
+    pass
