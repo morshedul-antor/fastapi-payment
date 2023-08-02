f6cf456fa7a5e4c970d862fcb4c16cdd6deac978
morshedul-antor <antor.morshedul@gmail.com>
2023-08-03 02:05:52 +0600

    add: payments route

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from .endpoints import auth, todo, users
+from .endpoints import auth, todo, users, payments
 
 api_router = APIRouter()
 
 # fmt: off
-api_router.include_router(auth.router, prefix='', tags=['Auth'])
-api_router.include_router(users.router, prefix='/users', tags=['Users'])
-api_router.include_router(todo.router, prefix='/todo', tags=['Todos'])
+# api_router.include_router(auth.router, prefix='', tags=['Auth'])
+# api_router.include_router(users.router, prefix='/users', tags=['Users'])
+# api_router.include_router(todo.router, prefix='/todo', tags=['Todos'])
+api_router.include_router(payments.router, prefix="/payment", tags=['Payments'])
